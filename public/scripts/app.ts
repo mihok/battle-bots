2735c0102be4622b38d772e88e4cc1b77c66e1b5
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2018-04-14 12:29:23 -0400

    Fixing blasterArm and making it register and shoot!

--- public/scripts/app.ts
+++ public/scripts/app.ts
@@ -1,13 +1,15 @@
 import { core } from './robot/core';
 import { HeadComponent } from './robot/components/head';
+import { BlasterArmComponent } from './robot/components/blasterArm';
 import * as CoreActions from './robot/core/actions';
 
 function onload() {
     // Load the core
 
     core.registerComponent(HeadComponent, 'head');
+    core.registerComponent(BlasterArmComponent, 'leftArm');
 
-    core.subscribeToState((state) => {
+    core.subscribeToState((state) => { 
         console.log("Core State", state);
     });
 
@@ -16,6 +18,10 @@ function onload() {
     let headActions = core.getComponentActions('head');
 
     core.command('head', new headActions.SeekAction());
+
+    let leftArmActions = core.getComponentActions('leftArm');
+
+    core.command('leftArm', new leftArmActions.ShootAction());
 }
 
 window.addEventListener('load', onload, false);
