732f4e6f467b3d598a60df5c60924b20d3614072
John Taylor <johntayl89@gmail.com>
2018-04-13 21:15:12 -0700

    Removing build

--- public/build-tsc/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("./robot/core");
-function onload() {
-    // Load the core
-    console.log("TESTING");
-    console.log(core_1.core.getActions());
-}
-window.addEventListener('load', onload, false);
