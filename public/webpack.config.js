c9759e4106769b277f4d191403afb9b716fb4194
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2018-04-16 22:35:56 -0400

    Adding async support

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  devtool: "source-map",
-  entry: './build-babel/app.js',
+  devtool: 'source-map',
+  entry: ['@babel/polyfill', './build-babel/app.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
